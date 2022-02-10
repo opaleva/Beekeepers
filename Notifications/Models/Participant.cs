57d280cd7ffb3166690a3b7338b3aa472d036889
opaleva <from.aldebaran@gmail.com>
2022-02-10 04:54:56 +0300

    MongoDB is being added

--- Notifications/Models/Participant.cs
+++ Notifications/Models/Participant.cs
@@ -9,14 +9,15 @@ public class Participant
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
+    public  string Id { get; set; } = String.Empty;
     [Required(ErrorMessage = "Анонимным участникам у нас не рады")]
-    public string Name { get; set; }
-    public string? City { get; set; }
+    public string Name { get; set; } = String.Empty;
+    public string? City { get; set; } 
     [Required(ErrorMessage = "Поле обязательно для заполнения")]
     [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage="Это не email")]
-    public string Email { get; set; }
+    public string Email { get; set; } = String.Empty;
     [Required(ErrorMessage = "Поле обязательно для заполнения")]
     [RegularExpression(@"^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d$", ErrorMessage="Это не номер телефона")]
-    public string Phone { get; set; }
-    public bool? Expected { get; set; }
+    public string Phone { get; set; } = String.Empty;
+    public bool Expected { get; set; }
 }
