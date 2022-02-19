5caaadd634f6b11e9c91bc07bab705298a1c6cb3
opaleva <from.aldebaran@gmail.com>
2022-02-19 03:01:09 +0300

    Conference.Core folder was added

--- Conference.Contracts/Repositories/IEventRepository.cs
+++ Conference.Contracts/Repositories/IEventRepository.cs
@@ -5,11 +5,11 @@ namespace Conference.Contracts.Repositories;
 
 public interface IEventRepository : IRepository<Event>
 {
-    Task<Event> GetBeekeeperByIdAsync(string eventId);
+    Task<Event> GetEventByIdAsync(string eventId);
 
-    Task CreateBeekeeperAsync(CreateOrUpdateEventDto model);
+    Task CreateEventAsync(CreateOrUpdateEventDto model);
 
-    Task<Event> UpdateBeekeeperAsync(string id, CreateOrUpdateEventDto model);
+    Task<Event> UpdateEventAsync(string id, CreateOrUpdateEventDto model);
 
-    Task DeleteBeekeeperAsync(string id);
+    Task DeleteEventAsync(string id);
 }
